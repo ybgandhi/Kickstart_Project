847c71311e8b110d1b418892236b3a81e76db94d
ybgandhi <gandhibyash@gmail.com>
2021-06-12 01:59:04 -0400

    index update

--- Web Page/logic.js
+++ Web Page/logic.js
@@ -17,4 +17,33 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 var link = "Resources/locations.geojson";
 
 // Create markers for all NFL stadiums
-L.marker([45.52, -122.67]).addTo(myMap);
+
+// Grab the data with d3
+d3.json(link).then(function(response) {
+
+    // Create a new marker cluster group
+    var markers = L.markerClusterGroup();
+  
+    // Loop through data
+    for (var i = 0; i < response.length; i++) {
+  
+      // Set the data location property to a variable
+      var location = response[i].location;
+  
+      // Check for location property
+      if (location) {
+  
+        // Add a new marker to the cluster group and bind a pop-up
+        markers.addLayer(L.marker([location.coordinates[1], location.coordinates[0]])
+          .bindPopup(response[i].descriptor));
+      }
+  
+    }
+  
+    // Add our marker cluster layer to the map
+    myMap.addLayer(markers);
+  
+  });
+
+
+
