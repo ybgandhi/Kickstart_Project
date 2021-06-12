8218bdbcb7cb379b700b9f0add60b0a3460e4e29
ybgandhi <gandhibyash@gmail.com>
2021-06-12 01:18:20 -0400

    web page - map updates

---
+++ Web Page/logic.js
@@ -0,0 +1,20 @@
+// Create an initial map object
+// Set the longitude, latitude, and the starting zoom level
+var myMap = L.map("map").setView([39.8283, -98.5795], 5);
+
+// Add a tile layer (the background map image) to our map
+// Use the addTo method to add objects to our map
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/dark-v10",
+  accessToken: API_KEY
+}).addTo(myMap);
+
+// link to get the geojson data.
+var link = "Resources/locations.geojson";
+
+// Create markers for all NFL stadiums
+L.marker([45.52, -122.67]).addTo(myMap);
