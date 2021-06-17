8a0d8cc3460d24192f9be2880e27a8eaecdc46b4
ybgandhi <gandhibyash@gmail.com>
2021-06-16 21:48:28 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,29 @@
 # imort dependencies
+from logging import debug
 from flask import Flask, jsonify
 
 #############
 #Flask set up
 #############
+
+app = Flask(__name__)
+
+#############
+#Flask Routes
+#############
+@app.route("api route")
+def dataset():
+    """Return data as JSON"""
+
+    return jsonify()
+
+@app.route("/")
+def welcome():
+    return (
+        f"Welcome to the NFL Dataset API<br/>"
+        f"Available Routes: <br/"
+        f"api route"
+    )
+if __name__ == "__main__":
+    app.run(debug=True)
+    
