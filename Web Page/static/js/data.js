8b430b10f005dcc5ab8f67b27044acb1e1c52e21
ybgandhi <gandhibyash@gmail.com>
2021-06-21 21:14:57 -0400

    organized repo directory

---
+++ Web Page/static/js/data.js
@@ -0,0 +1,45 @@
+// nfl teams dictionary
+
+var teams = [
+    {
+        team: "Jets",
+        city: "New York",
+        conf: "AFC",
+        stadium: "Metlife Stadium" 
+    }, {
+        team: "",
+        city: "",
+        conf: "",
+        stadium: ""
+    }, {
+        team: "",
+        city: "",
+        conf: "",
+        stadium: ""
+    }, {
+        team: "",
+        city: "",
+        conf: "",
+        stadium: ""
+    }, {
+        team: "",
+        city: "",
+        conf: "",
+        stadium: ""
+    }, {
+        team: "",
+        city: "",
+        conf: "",
+        stadium: ""
+    }, {
+        team: "",
+        city: "",
+        conf: "",
+        stadium: ""
+    }
+
+
+
+
+
+]
