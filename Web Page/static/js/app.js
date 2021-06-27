2f4b7af2014fee43dea86c27487e32a97467ff4c
ybgandhi <gandhibyash@gmail.com>
2021-06-26 21:18:30 -0400

    loaded JSON data to table

--- Web Page/static/js/app.js
+++ Web Page/static/js/app.js
@@ -2,27 +2,23 @@
 var tbody = d3.select("tbody");
 //var button = d3.select("filter-btn")
 
-var data = JSON.parse('./Web Page/static/data/Table.json');
-console.log(data);
- 
+d3.json("./static/data/Table.json", function(data){
+    console.log(data);
+    data.forEach(function(data){
+        var row = tbody.append("tr");
+        
+        row.append("td").text(data.Season);
+        row.append("td").text(data.Month);
+        row.append("td").text(data.Week);
+        row.append("td").text(data.Winner);
+        row.append("td").text(data.Home_Team);
+        row.append("td").text(data.Kickoff_Temp);
+        row.append("td").text(data.Weahter_Element);
+        row.append("td").text(data.Cloudiness);
+        row.append("td").text(data.Stadium);
+    });
 
-
-
-// function populateTable(){
-//     dataTable.localeCompare(data =>{
-//         var row = tbody.append("tr");
-//         row.append("td").text(data.Season);
-//         row.append("td").text(data.Month);
-//         row.append("td").text(data.Week);
-//         row.append("td").text(data.Kickoff_Time);
-//         row.append("td").text(data.Winner);
-//         row.append("td").text(data.Home_Team);
-//         row.append("td").text(data.Kickoff_Temp);
-//         row.append("td").text(data.Weahter_Element);
-//         row.append("td").text(data.Cloudiness);
-//         row.append("td").text(data.Stadium);
-//     });
-// }
-// populateTable(data);
+});
+//populateTable(dataTable);
 
 
