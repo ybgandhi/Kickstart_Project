7004ee88c8528164c2f85e71fdc0c50fc559f80a
ybgandhi <gandhibyash@gmail.com>
2021-07-09 23:09:38 -0400

    finish typing raw rain data in js for teams

--- Web Page/static/js/rain_teams.js
+++ Web Page/static/js/rain_teams.js
@@ -2,3 +2,21 @@ google.charts.load('current', {'packages':['corechart']});
       // Set a callback to run when the Google Visualization API is loaded.
 google.charts.setOnLoadCallback(drawChart);
 
+function drawChart() {
+    var data = google.visualization.arrayToDataTable([
+      ['Top 5 Cities', 'Rainy Games Played'],
+      ['Chicago',19],
+      ['Oakland',19],
+      ['Baltimore',17],
+      ['New England', 16],
+      ['Sleep',    7]
+    ]);
+
+    var options = {
+      title: 'My Daily Activities',
+      is3D: true,
+    };
+
+    var chart = new google.visualization.PieChart(document.getElementById('piechart_3d'));
+    chart.draw(data, options);
+  }
