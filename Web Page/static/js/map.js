e1ed9a6bad4cda906bbe61f5b3cbdb84d4f047cf
ybgandhi <gandhibyash@gmail.com>
2021-07-11 02:50:49 -0400

    update table click in js

--- Web Page/static/js/map.js
+++ Web Page/static/js/map.js
@@ -16,222 +16,18 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
   accessToken: API_KEY
 }).addTo(myMap);
 console.log(myMap)
-// link to get the geojson data.
-// var link = "locations.geojson";
-// L.geoJson(link).addTo(myMap);
-
-// d3.json(link).then(function(data) {
-//    L.geoJson(data, {
-//        onEachFeature: function(feature, layer) {
-//            layer.on({
-//               click: function(event) {
-//                   myMap.fitBounds(event.target.getBounds()); 
-//               }
-//            });
-//            layer.bindPopup(feature.properties.Stadium);
-//        }
-//    }).addTo(myMap);
-// });
-
-var stadiums = [
-  {
-    name: "Soldier Field",
-    locations: [41.8135,-74.0745],
-    city: "Chicago",
-    team: "Bears",
-    conf: "NFC"
-  },
-  {
-    name: "MetLife Stadium",
-    locations: [40.8135,-74.0745],
-    city: "New York",
-    team: "Jets",
-    conf: "AFC"
-  },
-  {
-    name: "MetLife Stadium",
-    locations: [40.8135,-74.1745],
-    city: "New York",
-    team: "Giants",
-    conf: "NFC",
-  },
-  {
-    name: "Lambeau Field",
-    locations: [44.5013,-88.0622],
-    city: "Green Bay",
-    team: "Packers",
-    conf: "NFC"
-  },
-  {
-    name: "State Farm Stadium",
-    locations: [33.5276,-112.2626],
-    city: "Arizona",
-    team: "Cardinals",
-    conf: "NFC"
-  },
-  {
-    name: "M&T Bank Stadium",
-    locations: [39.2780,-76.6227],
-    city: "Baltimore",
-    team: "Ravens",
-    conf: "AFC"
-  },
-  {
-    name: "New Era Field",
-    locations: [42.7738,-78.7870],
-    city: "Buffalo",
-    team: "Bills",
-    conf: "AFC"
-  },
-  {
-    name: "Bank of America Stadium",
-    locations: [35.2258,-80.8528],
-    city: "Carolina",
-    team: "Panthers",
-    conf: "NFC"
-  },
-  {
-    name: "Paul Brown Stadium",
-    locations: [39.0955,-84.5161],
-    city: "Cincinnati",
-    team: "Bengals",
-    conf: "AFC"
-  },
-  {
-    name: "FirstEnergy Stadium",
-    locations: [41.5061,-81.6995],
-    city: "Cleveland",
-    team: "Browns",
-    conf: "AFC"
-  },
-  {
-    name: "Empower Field at Mile High",
-    locations: [39.7439,-105.0201],
-    city: "Denver",
-    team: "Broncos",
-    conf: "AFC"
-  },
-  {
-    name: "Lucas Oil Stadium",
-    locations: [39.7601,-86.1639],
-    city: "Indianapolis",
-    team: "Colts",
-    conf: "AFC"
-  },
-  {
-    name: "TIAA Bank Field",
-    locations: [30.3239,-81.6373],
-    city: "Jacksonville",
-    team: "Jaguars",
-    conf: "AFC"
-  },
-  {
-    name: "Arrowhead Stadium",
-    locations: [39.0489,-94.4839],
-    city: "Kansas City",
-    team: "Cheifs",
-    conf: "AFC"
-  },
-  {
-    name: "Hard Rock Stadium",
-    locations: [25.9580,-80.2359],
-    city: "Miami",
-    team: "Dolphins",
-    conf: "AFC"
-  },
-  {
-    name: "Gillette Stadium",
-    locations: [42.0909,-71.2643],
-    city: "New England",
-    team: "Patriots",
-    conf: "AFC"
-  },
-  {
-    name: "Oakland Coliseum",
-    locations: [37.7516,-122.2005],
-    city: "Oakland",
-    team: "Raiders",
-    conf: "AFC"
-  },
-  {
-    name: "Lincoln Financial Field",
-    locations: [39.9001,-75.1679],
-    city: "Philadelphia",
-    team: "Eagles",
-    conf: "NFC"
-  },
-  {
-    name: "Heinz Field",
-    locations: [40.4468,-80.0158],
-    city: "Pittsburgh",
-    team: "Steelers",
-    conf: "AFC"
-  },
-  {
-    name: "Edward Jones Dome",
-    locations: [38.6263,-90.1865],
-    city: "St. Louis",
-    team: "Rams",
-    conf: "NFC"
-  },
-  {
-    name: "Qualcomm Stadium",
-    locations: [32.7831,-117.1196],
-    city: "San Diego",
-    team: "Chargers",
-    conf: "AFC"
-  },
-  {
-    name: "Levis Stadium",
-    locations: [37.4032,-121.9697],
-    city: "San Francisco",
-    team: "49ners",
-    conf: "NFC"
-  },
-  {
-    name: "Lumen Field",
-    locations: [47.5952,-122.3316],
-    city: "Seattle",
-    team: "Seahawks",
-    conf: "NFC"
-  },
-  {
-    name: "Raymond James Stadium",
-    locations: [27.9759,-82.5033],
-    city: "Tampa Bay",
-    team: "Bucs",
-    conf: "NFC"
-  },
-  {
-    name: "Nissan Stadium",
-    locations: [36.1665,-86.7713],
-    city: "Tennessee",
-    team: "Titans",
-    conf: "AFC"
-  },
-  {
-    name: "FedEx Field",
-    locations: [38.9076,-76.8645],
-    city: "Washington",
-    team: "Football Team",
-    conf: "NFC"
-  },
-
-];
 
 
-var inputCity = d3.select("#hometown").property("value");
-var inputConf = d3.select("#conf").property("value");
-
-// insert pictures for pop - ups
+// insert markers and their functionalities
 function markers () {
 
   for (var i = 0; i < stadiums.length; i++) {
 
     var marker = L.marker(stadiums[i].locations, {
-      name: stadiums[i].team,
+      team: stadiums[i].team,
       city: stadiums[i].city,
-      
+      conf: stadiums[i].conf
+
     })
 
     // bind popup to chow picture of stadium and name of stadium 
@@ -240,14 +36,26 @@ function markers () {
 
     // click event to populate table on the right side
     .on({
-      click: function (team_infro) {
+      click: function (e) {
 
         // make table appear / disappear on click
         //if (document.getElementById("tableinfoID").style.display === "none")
         document.getElementById("tableinfoID").style.display = "block";
           //else document.getElementById("tableinfoID").style.display = "none";
         
-          var name = d3.select("team")
+        // console team name of selected marker
+        //console.log(e.target.options.team);
+
+        // add team information to table
+        var team = document.getElementById("team");
+        team.innerHTML = e.target.options.team;
+        var city = document.getElementById("city");
+        city.innerHTML = e.target.options.city;
+        var conf = document.getElementById("conf");
+        conf.innerHTML = e.target.options.conf;
+
+
+        
 
       }
     });
